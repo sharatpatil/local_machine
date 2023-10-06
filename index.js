08b5857673d65ac0ae1b118485543b7e7905dd28
sharath <sharathkumarpatil06@gmail.com>
2023-10-06 00:33:04 -0700

    code init

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const sql = require('mssql');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 3000;
+const os = require('os');
+
+const { exec } = require('child_process');
+const { request } = require('http');
+
+// Parse JSON bodies
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+	res.status(200);
+	res.send("Welcome to root URL of Server");
+});
+
+
+
+const config = {
+    user: 'sa',
+    password: 'sa123',
+    server: 'SQCPACKSYSTEM\SQLEXPRESS',
+    database: 'test',
+    options: {           
+        encrypt: false
+    }
+};
+
+sql.connect(config)
+  .then(() => {
+    console.log('Connected to the MSSQL database.')
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+  });
+
+  app.post('/devices', async (req, res) => {
+    try {
+      const deviceData = req.body;
+
+      const {
+        deviceName,
+        deviceNumber1,
+        deviceNumber2,
+        deviceNumber3,
+        deviceNumber4,
+        parameterName1,
+        parameterName2,
+        parameterName3,
+        parameterName4,
+        parameterName5,
+        parameterName6,
+        parameterName7,
+        parameterName8,
+        parameterName9,
+        parameterName10,
+        parameter1,
+        parameter2,
+        parameter3,
+        parameter4,
+        parameter5,
+        parameter6,
+        parameter7,
+        parameter8,
+        parameter9,
+        parameter10,
+        UserId
+      } = deviceData;
+
+      const device_id =  'DEV' + (Math.floor(Math.random() * 9000) + 1000).toString() 
+      
+      const query = `INSERT INTO devices (device_id, device_name,device_number1, device_number2, device_number3, device_number4, parameter_name1, parameter_name2, parameter_name3, parameter_name4, parameter_name5, parameter_name6, parameter_name7,parameter_name8, parameter_name9, parameter_name10, parameter1, parameter2, parameter3, parameter4, parameter5, parameter6, parameter7, parameter8, parameter9, parameter10, user_id) 
+                     VALUES ('${device_id}', '${deviceName}', '${deviceNumber1}', '${deviceNumber2}', '${deviceNumber3}', '${deviceNumber4}',  '${parameterName1}', '${parameterName2}', '${parameterName3}', '${parameterName4}', '${parameterName5}', '${parameterName6}', '${parameterName7}', '${parameterName8}', '${parameterName9}', '${parameterName10}', '${parameter1}', '${parameter2}', '${parameter3}', '${parameter4}', '${parameter5}', '${parameter6}', '${parameter7}', '${parameter8}', '${parameter9}', '${parameter10}', ${UserId})`;
+  
+
+
+    
+      const result = await sql.query(query)
+    //   console.log('Data inserted successfully:', result);
+  
+      // res.status(200).json({ message: 'Data inserted successfully' });
+      res.json(deviceData)
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
+  
+
+  // Execute the ipconfig command
+  exec('ipconfig', (error, stdout) => {
+    if (error) {
+      console.error(`Error executing ipconfig: ${error}`);
+      return;
+    }
+  
+    // Output the ipconfig results
+    console.log(stdout);
+  });
+  
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT + os.hostname)
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
+
