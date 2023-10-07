6411a0baa3f576503abd4e28dd6b8edbb62736ef
sharath <sharathkumarpatil06@gmail.com>
2023-10-07 03:43:46 -0700

    updated

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const os = require('os');
 const { exec } = require('child_process');
 const { request } = require('http');
 
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+
+const emailTemplate = fs.readFileSync('mail_templates/device_created.html', 'utf-8');
+
 // Parse JSON bodies
 app.use(bodyParser.json());
 
@@ -21,7 +26,7 @@ app.get('/', (req, res)=>{
 const config = {
     user: 'sa',
     password: 'sa123',
-    server: 'SQCPACKSYSTEM\SQLEXPRESS',
+    server: 'SQCPACKSYSTEM\\SQLEXPRESS',
     database: 'test',
     options: {           
         encrypt: false
@@ -69,7 +74,65 @@ sql.connect(config)
         UserId
       } = deviceData;
 
-      const device_id =  'DEV' + (Math.floor(Math.random() * 9000) + 1000).toString() 
+      // const device_id =  'DEV' + (Math.floor(Math.random() * 9000) + 1000).toString() 
+      
+      // Retrieve parameter limitations from the database
+    const query1 = 'SELECT parameter_name, upper_limit, lower_limit FROM Config';
+    const result1 = await sql.query(query1);
+    const limitations = result1.recordset;
+
+    
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'sqcpack.co.in@gmail.com',
+    pass: 'sxiyujgfvijcwdrv'
+  }
+});
+
+
+    
+    limitations.forEach((limit) => {
+      const paramName = limit.parameter_name;
+      const paramValue = deviceData[paramName];
+      const upperLimit = limit.upper_limit;
+      const lowerLimit = limit.lower_limit;
+
+      if (paramValue < lowerLimit || paramValue > upperLimit) {
+        console.log('Out of range',lowerLimit, deviceData[paramName], upperLimit)
+
+        
+        const emailBody = emailTemplate
+          .replace('{{deviceNumber1}}', deviceNumber1 || '')
+          .replace('{{deviceNumber2}}', deviceNumber2 || '')
+          .replace('{{deviceNumber3}}', deviceNumber3 || '')
+          .replace('{{deviceNumber4}}', deviceNumber4 || '')
+          .replace('{{paramName}}', paramName || '')
+          .replace('{{paramValue}}', paramValue || '');
+
+        const mailOptions = {
+          from: 'sqcpack.co.in@gmail.com',
+          to: ['sharathkumarpatil06@gmail.com','support@sqcpack.co.in','nandapqsystems@gmail.com',' product@pqsi.co.in'],
+          subject: 'Alert: Data Point Outside the Limits',
+          html: emailBody
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+
+      }
+    });
+
+   
+
+    // If validation passes, proceed with inserting data into the database
+    const device_id =
+      'DEV' + (Math.floor(Math.random() * 9000) + 1000).toString();
       
       const query = `INSERT INTO devices (device_id, device_name,device_number1, device_number2, device_number3, device_number4, parameter_name1, parameter_name2, parameter_name3, parameter_name4, parameter_name5, parameter_name6, parameter_name7,parameter_name8, parameter_name9, parameter_name10, parameter1, parameter2, parameter3, parameter4, parameter5, parameter6, parameter7, parameter8, parameter9, parameter10, user_id) 
                      VALUES ('${device_id}', '${deviceName}', '${deviceNumber1}', '${deviceNumber2}', '${deviceNumber3}', '${deviceNumber4}',  '${parameterName1}', '${parameterName2}', '${parameterName3}', '${parameterName4}', '${parameterName5}', '${parameterName6}', '${parameterName7}', '${parameterName8}', '${parameterName9}', '${parameterName10}', '${parameter1}', '${parameter2}', '${parameter3}', '${parameter4}', '${parameter5}', '${parameter6}', '${parameter7}', '${parameter8}', '${parameter9}', '${parameter10}', ${UserId})`;
